12cbd80c3b878bae74124a943647407f93960858
boiko-m <maks1102b@gmail.com>
2016-10-30 11:16:06 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 "use strict";
 
+var id = '755a3e82ea9b0998bc6d83891d7bca33';
+var limit = '100';
+var txtsearch = "mark";
+
 let getPlayers = () => {
   $.ajax({
      type: 'GET',
-     url:'https://api.worldofwarplanes.ru/wowp/account/list/?application_id=755a3e82ea9b0998bc6d83891d7bca33&limit=100&type=startswith&search=mark',
+     url:'https://api.worldofwarplanes.ru/wowp/account/list/?application_id=' + id + '&limit=' + limit + '&type=startswith&search=' + window.txtsearch,
      success:function(channel) {
+       let template = Handlebars.compile( $('#template').html()  );
+       $('.updates').empty().append( template(channel)  );
        console.log("update");
      },
      error:function() {
@@ -12,3 +18,30 @@ let getPlayers = () => {
      }
    });
 }
+
+$(document).ready(function() {
+  getPlayers();
+
+  $('body').append('<div id="ajaxBusy"><p><img src="images/loader.gif"></p></div>');
+  $('#ajaxBusy').css({
+    display:"none",
+    margin:"0px",
+    textAlign:"center",
+    height:"100vh",
+    paddingLeft:"0px",
+    paddingRight:"0px",
+    paddingTop:"50vh",
+    backgroundColor:"rgba(255,255,255,0.7)",
+    paddingBottom:"0px",
+    position:"absolute",
+    right:"0",
+    top:"0",
+    width:"100%"
+  });
+});
+
+$(document).ajaxStart(function(){
+  $('#ajaxBusy').show();
+}).ajaxStop(function(){
+  $('#ajaxBusy').hide();
+});
